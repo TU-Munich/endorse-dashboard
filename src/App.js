da17bd2e94c47d4c7eacadb4d369b71ebdf5af51
Marco Antonio Salazar González <marco.salazar93@gmail.com>
2018-12-14 16:24:01 -0600

    Remove unused import from App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'filepond/dist/filepond.min.css';
 import Home from './containers/Home/Home';
 import Dashboard from './containers/Dashboard/Dashboard';
