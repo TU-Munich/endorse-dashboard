afc0f262108bf29dff9bfb71bb53432ceccaa228
Marco Antonio Salazar González <marco.salazar93@gmail.com>
2018-12-15 10:10:47 -0600

    Remove unnecessary strict keyword.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const port      = process.env.PORT || '3000';
 const JwtSecret = process.env.JWT_SECRET  || 'very secret secret';
 const nlpServiceBaseUrl = process.env.NLP_SERVICE_BASE_URL || 'http://localhost:5000';
