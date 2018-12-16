cf96366239dfb8ad4533a8a154de25305831a353
Marco Antonio Salazar González <marco.salazar93@gmail.com>
2018-12-16 17:03:50 -0600

    Reformat code to indent files with 2 spaces

--- src/components/Dashboard/UploadFiles/UploadFiles.js
+++ src/components/Dashboard/UploadFiles/UploadFiles.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components';
 import Card from '@material-ui/core/Card';
-import { FilePond, File } from 'react-filepond';
-import { nlpServiceBaseUrl } from '../../../config';
+import {FilePond, File} from 'react-filepond';
+import {nlpServiceBaseUrl} from '../../../config';
 
 const ContainerDiv = styled.div`
   display: flex;
@@ -22,59 +22,59 @@ const UploadContainer = styled.div`
 
 class UploadFiles extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            loading: false,
-            data: [],
-            files: []
-        };
+    this.state = {
+      loading: false,
+      data: [],
+      files: []
+    };
 
-        this.clickState = this.clickState.bind(this);
+    this.clickState = this.clickState.bind(this);
+  }
+
+  clickState() {
+    this.setState({data: 'Test'});
+    console.log(this.state.files);
+  }
+
+  handleFilePondInit() {
+    console.log('FilePond instance has initialised', this.pond);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (<h2>Loading...</h2>);
     }
 
-    clickState() {
-        this.setState({data: 'Test'});
-        console.log(this.state.files);
-    }
-
-    handleFilePondInit() {
-      console.log('FilePond instance has initialised', this.pond);
-    }
-
-    render() {
-      if (this.state.loading) {
-        return (<h2>Loading...</h2>);
-      }
-
-      return (
-        <ContainerDiv>
-          <Card style={{height:"300%", padding:"10px 10px 10px"}}>
-            <h1>Local File Processing</h1>
-            <p>Please select or drag the files you want to fetch into the processing system</p>
-            <UploadContainer >
-              <FilePond ref={ref => this.pond = ref}
-                        allowMultiple={true}
-                        maxFiles={10}
-                        server={nlpServiceBaseUrl + "/files/upload"}
-                        oninit={() => this.handleFilePondInit() }
-                        onupdatefiles={(fileItems) => {
-                          this.setState({
-                            files: fileItems.map(fileItem => fileItem.file)
-                          });
-                        }}>
-                {
-                  this.state.files.map(file => (
-                    <File key={file} src={file} origin="local" />
-                  ))
-                }
-              </FilePond>
-            </UploadContainer>
-          </Card>
-        </ContainerDiv>
-      );
-    }
+    return (
+      <ContainerDiv>
+        <Card style={{height: "300%", padding: "10px 10px 10px"}}>
+          <h1>Local File Processing</h1>
+          <p>Please select or drag the files you want to fetch into the processing system</p>
+          <UploadContainer>
+            <FilePond ref={ref => this.pond = ref}
+                      allowMultiple={true}
+                      maxFiles={10}
+                      server={nlpServiceBaseUrl + "/files/upload"}
+                      oninit={() => this.handleFilePondInit()}
+                      onupdatefiles={(fileItems) => {
+                        this.setState({
+                          files: fileItems.map(fileItem => fileItem.file)
+                        });
+                      }}>
+              {
+                this.state.files.map(file => (
+                  <File key={file} src={file} origin="local"/>
+                ))
+              }
+            </FilePond>
+          </UploadContainer>
+        </Card>
+      </ContainerDiv>
+    );
+  }
 }
 
 export default UploadFiles;
