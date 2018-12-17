7765f57733d07b928962e122cff01329bb937e94
Marco Antonio Salazar González <marco.salazar93@gmail.com>
2018-12-17 08:06:17 -0600

    Add particle canvas to login screen

--- src/containers/Login/LoginContainer.js
+++ src/containers/Login/LoginContainer.js
@@ -1,19 +1,39 @@
 import React, {Component} from 'react';
 import Login from '../../components/Login/Login'
 import styled from 'styled-components';
+import Particles from "react-particles-js";
+import ParticlesConfig from "../../constants/ParticlesConfig";
+
+
+const ParticlesCanvas = styled(Particles)`
+  position: absolute; 
+  top: 0; right: 0; bottom: 0; left: 0;
+  background-color: rgb(255, 255, 255);
+  width: 100%;
+  z-index: 2;
+`;
 
 const LoginWrapper = styled.div`
-  position: relative;
-  text-align: center;
-  padding: 20vh;
+  align-items: center;
+  background-color: rgba(224, 224, 224, 0.5);
+  display: -webkit-flex;
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  top: 0; right: 0; bottom: 0; left: 0;
+  width: 100%;
+  z-index: 2;
 `;
 
 class LoginContainer extends Component {
   render() {
     return (
-      <LoginWrapper className="login-wrapper">
-        <Login/>
-      </LoginWrapper>
+      <div>
+        <ParticlesCanvas params={ParticlesConfig}/>
+        <LoginWrapper className="login-wrapper">
+          <Login/>
+        </LoginWrapper>
+      </div>
     );
   }
 }
