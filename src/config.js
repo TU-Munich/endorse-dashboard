81c845f75749ce0c8d6cdff53ef2be612ed31db8
iambigmomma <iambigmomma@gmail.com>
2019-02-11 22:03:50 +0100

    Revised the IP into localhost

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const port      = process.env.PORT || '3000';
 const JwtSecret = process.env.JWT_SECRET  || 'very secret secret';
-const nlpServiceBaseUrl = process.env.NLP_SERVICE_BASE_URL || 'http://35.197.219.92:3002/api/1'; // when using data nlp in docker compose
+const nlpServiceBaseUrl = process.env.NLP_SERVICE_BASE_URL || 'http://localhost:3002/api/1'; // when using data nlp in docker compose
+//const nlpServiceBaseUrl = process.env.NLP_SERVICE_BASE_URL || 'http://35.197.219.92:3002/api/1'; // when using data nlp in docker compose
 //const nlpServiceBaseUrl = process.env.NLP_SERVICE_BASE_URL || 'http://localhost:5000/api/1';
 
 module.exports = {
